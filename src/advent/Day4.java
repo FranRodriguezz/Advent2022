1d989d922d3cbf8a5d0bcac6770447052f9b593a
Franco Ezequiel Rodriguez <frrodriguez@fi.uba.ar>
2022-12-24 17:02:00 -0300

    Day 4 completed

--- src/advent/Day4.java
+++ src/advent/Day4.java
@@ -20,17 +20,16 @@ public class Day4 {
 
 
     //Part 2: **********************************************************************************************************
-    public static boolean compareRanges2(String[] str1, String[] str2){
+    public static boolean compareRanges2(String[] str1, String[] str2) {
         int[] range1 = new int[2];
         int[] range2 = new int[2];
         for (int i = 0; i < 2; i++) {
             range1[i] = Integer.parseInt(str1[i]);
             range2[i] = Integer.parseInt(str2[i]);
         }
-        if(range1[0] >= range2[0] && range2[1] >= range1[0]){
+        if (range1[0] >= range2[0] && range2[1] >= range1[0]) {
             return true;
-        }
-        else return range1[0] <= range2[0] && range1[1] >= range2[0];
+        } else return range1[0] <= range2[0] && range1[1] >= range2[0];
     }
 
     //Part 1: **********************************************************************************************************
@@ -45,7 +44,6 @@ public class Day4 {
     }
 
     public static boolean contains(String str) {
-        boolean cont = false;
         String[] aux = str.split(",");
         return compareStrings(aux[0], aux[1]);
     }
@@ -53,7 +51,7 @@ public class Day4 {
     public static boolean compareStrings(String str1, String str2) {
         String[] range1 = str1.split("-");
         String[] range2 = str2.split("-");
-        return compareRanges2(range1, range2); //change between compare ranges and compare ranges 2
+        return compareRanges2(range1, range2); //switch between compare ranges and compare ranges 2
     }
 
     public static boolean compareRanges(String[] str1, String[] str2) {
